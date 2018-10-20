39457d1ffb2d0869dab5acc61b2270501b0a3637
Nazhari <32801301+Nazhari@users.noreply.github.com>
2018-10-20 11:57:14 -0400

    Add files via upload

---
+++ despacito.ino
@@ -0,0 +1,12 @@
+void setup() {
+  Serial.begin(1200);
+}
+
+void loop() {
+  Serial.println("Des");
+  delay(1000);
+  Serial.println("pa");
+  delay(1000);
+  Serial.println("cito");
+  delay(1000);
+}
